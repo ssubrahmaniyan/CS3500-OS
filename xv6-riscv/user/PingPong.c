27a85a59c6f835e7d488352aa2144af80f9dc02a
ssubrahmaniyan <subrahmaniyansanjeev@gmail.com>
2025-08-14 11:27:14 +0530

    Added PingPong.c

--- xv6-riscv/user/PingPong.c
+++ xv6-riscv/user/PingPong.c
@@ -1,43 +1,43 @@
+// pingpong problem 
+//
+
 #include "kernel/types.h"
 #include "user/user.h"
 #include "kernel/fcntl.h"
 
-int main(){
-// ping pong a message
+int main(int argc, char *argv[]){
+  int p1[2], p2[2];
+  char buf1[4], buf2[4];
 
-  int p1[2];
-  int p2[2];
   pipe(p1);
   pipe(p2);
   
-  char buf1[64];
-  char buf2[64];
+  int n;
+  if((n = fork()) == 0){// child here 
+    close(p1[1]);
+    close(p2[0]);
 
-  // in parent space 
-  printf("Parent: sending message: Hi!\n");
-  close(0);
-  dup(p1[1]);
-  write(0, "Hi!\n", 4);
-  
-  // close the unused ends
-  if(fork() == 0){
-    // child space
-    close(0);
-    dup(p1[0]);
-    read(0, buf1, 4);
-    printf("Child: received message: %s", buf1);
-    close(0);
-    dup(p2[1]);
-    printf("Child: sending message: %s", buf1);
-    write(0, buf1, 4);
+    read(p1[0], buf1, sizeof(buf1));
+    printf("%d: received %s from parent\n", getpid(), buf1);
+
+    close(p1[0]);
+
+    write(p2[1], "Pong", 4);
+    close(p2[1]);
     exit(0);
   }
-  else{
+  else{// parent here
+    // closing unused ends of the two pipes
+    close(p1[0]);
+    close(p2[1]);
+    // putting data on pipe 1
+    write(p1[1], "Ping", 4);
+    close(p1[1]);
+
+    read(p2[0], buf2, sizeof(buf2));
+    printf("%d: received %s from child\n", getpid(), buf2);
+    close(p2[0]);
     wait(0);
-    close(0);
-    dup(p2[0]);
-    read(0, buf2, 4);
-    printf("Parent: received message: %s", buf2);
     exit(0);
   }
 }
